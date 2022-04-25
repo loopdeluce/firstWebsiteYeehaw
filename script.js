46a5a7792e29d5a5528514a547590739db87dbd4
Lucy <lucy.d.bullen@gmail.com>
2022-04-25 12:59:19 -0600

    Update links bar formatting and password protect resume

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 // const h2 = document.createElement("h2");
 // h2.textContent = "This content added by JavaScript";
 // document.querySelector("body").appendChild(h2);
+
+const resume = document.querySelector("#links #resume");
+
+resume.addEventListener("click", password);
+
+function password() {
+    var password= prompt('Enter the password to view resume:');
+    if(password === "Bullen2022") {
+        window.open("./Bullen Lucy Resume Apr 2019.pdf");
+    }
+    else {
+        alert("Incorrect password!")
+    }
+};
+
+// const 
